4cf4ce3dce2230d8a6d704f84f15dd0ba993ea27
Darek <dardman88@gmail.com>
2020-05-05 16:04:38 +0200

    Create form and custom button components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HomePage from "./pages/homepage/homepage.component";
 import { Switch, Route } from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 import "./App.css";
 
@@ -16,6 +17,9 @@ function App() {
             </Route>
             <Route path="/shop">
                <ShopPage />
+            </Route>
+            <Route path="/signin">
+               <SignInAndSignUpPage />
             </Route>
          </Switch>
       </div>
