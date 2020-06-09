2ef6cbdf0ae08f7b54573406b8146341fe13c455
Darek <dardman88@gmail.com>
2020-06-09 18:04:54 +0200

    Add redux, implement userReducer and userActions to Header and App components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import HomePage from "./pages/homepage/homepage.component";
 import { Switch, Route } from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
+import { useDispatch } from "react-redux";
+import { setCurrentUser } from "./redux/user/user.actions";
 
 import "./App.css";
 
 function App() {
-   const [currentUser, setCurrentUser] = useState(null);
+   const dispatch = useDispatch();
 
    useEffect(() => {
       const unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
@@ -17,18 +19,18 @@ function App() {
             const userRef = await createUserProfileDocument(userAuth);
 
             userRef.onSnapshot(snapShot => {
-               setCurrentUser({ id: snapShot.id, ...snapShot.data() });
+               dispatch(setCurrentUser({ id: snapShot.id, ...snapShot.data() }));
             });
          } else {
-            setCurrentUser(userAuth);
+            dispatch(setCurrentUser(userAuth));
          }
       });
       return () => unsubscribeFromAuth();
-   }, []);
+   }, [dispatch]);
 
    return (
       <div>
-         <Header currentUser={currentUser} />
+         <Header />
          <Switch>
             <Route exact path="/">
                <HomePage />
