b97ff44119ead56731bf298056f1ef779410ad1f
Darek <dardman88@gmail.com>
2020-06-10 13:50:37 +0200

    Update shop data and add cart utils to group items in cart reducer

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,16 +1,27 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+
+import CustomButton from "../custom-button/custom-button.component";
+import { addItem } from "../../redux/cart/cart.actions";
 
 import "./collection-item.styles.scss";
-import CollectionPreview from "../collection-preview/collection-preview.component";
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
-   <div className="collection-item">
-      <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
-      <div className="collection-footer">
-         <span className="name">{name}</span>
-         <span className="price">{price}</span>
+const CollectionItem = ({ item }) => {
+   const { name, price, imageUrl } = item;
+   const dispatch = useDispatch();
+
+   return (
+      <div className="collection-item">
+         <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
+         <div className="collection-footer">
+            <span className="name">{name}</span>
+            <span className="price">{price}</span>
+         </div>
+         <CustomButton onClick={() => dispatch(addItem(item))} inverted>
+            Add to cart
+         </CustomButton>
       </div>
-   </div>
-);
+   );
+};
 
 export default CollectionItem;
