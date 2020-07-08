c561924f8f572b636f6fbfe097de6b571ab96980
Darek <dardman88@gmail.com>
2020-07-08 14:28:39 +0200

    Move logic from shop component to redux and use redux thunk

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -4,40 +4,29 @@ import { Route, useRouteMatch } from "react-router-dom";
 import CollectionsOverview from "../../components/collection-overview/collection-overview.component";
 import CollectionPage from "../collection/collection.component";
 
-import { firestore, convertCollectionsSnapshotToMap } from "../../firebase/firebase.utils";
+import { useSelector, useDispatch } from "react-redux";
 
-import { useDispatch } from "react-redux";
-
-import { updateCollections } from "../../redux/shop/shop.actions";
+import { fetchCollectionsStartAsync } from "../../redux/shop/shop.actions";
+import { selectIsCollectionFetching } from "../../redux/shop/shop.selectors";
 
 import WithSpinner from "../../components/with-spinner/with-spinner.component";
-
-import useToggler from "../../components/use-toggler/use-toggler.component";
 
 const ShopPage = () => {
    const match = useRouteMatch();
    const dispatch = useDispatch();
-   const [isLoading, toggle] = useToggler();
+   const isCollectionFetching = useSelector(selectIsCollectionFetching);
 
    useEffect(() => {
-      const collectionRef = firestore.collection("collections");
-
-      const unsubscribeFromSnapshot = collectionRef.onSnapshot(snapShot => {
-         const collectionsMap = convertCollectionsSnapshotToMap(snapShot);
-         dispatch(updateCollections(collectionsMap));
-         toggle();
-      });
-
-      return () => unsubscribeFromSnapshot();
-   }, [dispatch, toggle]);
+      dispatch(fetchCollectionsStartAsync());
+   }, [dispatch]);
 
    return (
       <div className="shop-page">
          <Route exact path={`${match.path}`}>
-            <CollectionsOverview />
+            {isCollectionFetching ? <CollectionsOverview /> : <WithSpinner />}
          </Route>
          <Route path={`${match.path}/:collectionId`}>
-            {isLoading ? <CollectionPage /> : <WithSpinner />}
+            {isCollectionFetching ? <CollectionPage /> : <WithSpinner />}
          </Route>
       </div>
    );
